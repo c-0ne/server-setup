39e634c50f40a0888dbdfc3d922e96e498cc55f5
c-0ne <57237675+c-0ne@users.noreply.github.com>
2026-01-31 20:56:36 +0100

    Update setupServer.sh

--- setupServer.sh
+++ setupServer.sh
@@ -4,7 +4,8 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-apt update
-apt install -y ncdu htop git curl ranger nano micro restic rsync fish tmux wget
-curl https://raw.githubusercontent.com/sinelaw/fresh/refs/heads/master/scripts/install.sh | sh
+sudo apt update
+sudo apt install -y ncdu htop git curl ranger nano micro restic rsync fish tmux wget
+sudo curl https://raw.githubusercontent.com/sinelaw/fresh/refs/heads/master/scripts/install.sh | sh
 chsh -s /usr/bin/fish
+
