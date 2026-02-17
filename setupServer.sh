984067418e950f9ab93806eccf18d9387dc29930
c-0ne <57237675+c-0ne@users.noreply.github.com>
2026-02-17 16:56:28 +0100

    added fd-find and ask for docker

--- setupServer.sh
+++ setupServer.sh
@@ -1,11 +1,21 @@
-#  proove if sudo
+# proove if sudo
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
 
 sudo apt update
-sudo apt install -y ncdu htop git curl ranger nano micro restic rsync fish tmux wget
+sudo apt install -y ncdu htop git curl ranger nano micro restic rsync fish tmux wget fd-find
 sudo curl https://raw.githubusercontent.com/sinelaw/fresh/refs/heads/master/scripts/install.sh | sh
 chsh -s /usr/bin/fish
+
+# ask to install docker
+read -p "Do you want to install docker? (y/n) " -n 1 -r
+echo    # move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sh get-docker.sh
+    rm get-docker.sh
+fi
 
